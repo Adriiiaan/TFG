88f5bdec4cee3fc151bf7d5362dc40a6483605e7
Adrián Cisneros Rascón <91887198+Adriiiaan@users.noreply.github.com>
2023-10-24 09:51:40 +0200

    controller

---
+++ backend/01_microservicio_clientes/src/main/java/com/market/service/ClientesServicelmpl.java
@@ -0,0 +1,31 @@
+package com.market.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.market.model.Cliente;
+import com.market.repository.ClientesRepository;
+@Service
+public class ClientesServicelmpl implements ClientesService {
+
+	@Autowired
+	ClientesRepository clienteRepository;
+	
+	
+	@Override
+	public Cliente auenticarCliente(String usuario, String password) {
+	 
+		return clienteRepository.findByUsuarioAndPassword(usuario, password);
+	}
+
+	@Override
+	public boolean registrarCliente(Cliente cliente) {
+		
+		if(clienteRepository.findById(cliente.getUsuario())!=null){		
+		return false;
+		}
+		clienteRepository.save(cliente);
+		return true;
+	}
+
+}
