6528266b80272f15f548e7a0ce55ae7474b2ea35
Adrián Cisneros Rascón <91887198+Adriiiaan@users.noreply.github.com>
2023-12-18 20:19:10 +0100

    PASARELA CON CAMPOS

--- angular/marketclient/src/app/controller/pasarela-pago/pasarela-pago.component.ts
+++ angular/marketclient/src/app/controller/pasarela-pago/pasarela-pago.component.ts
@@ -1,9 +1,7 @@
-import { ProcesarPedidoService } from './../../service/procesar-pedido.service';
 import { Component, OnInit } from '@angular/core';
 import { DataSharingService } from 'src/app/service/data-sharing.service';
 import { CestaItem } from 'src/app/model/CestaItem';
-import { Categoria } from 'src/app/model/Categoria';
-import { Producto } from 'src/app/model/Producto';
+import { ProcesarPedidoService } from './../../service/procesar-pedido.service';
 import { MenuComponent } from '../menu/menu.component';
 
 @Component({
@@ -14,17 +12,16 @@ import { MenuComponent } from '../menu/menu.component';
 export class PasarelaPagoComponent implements OnInit {
 
   cesta: CestaItem[];
+  nombre: string;
+  numero: string;
+  expiracion: string;
+  cvv: string;
 
-  constructor(private dataSharingService: DataSharingService,
+  constructor(
+    private dataSharingService: DataSharingService,
     private menuComponent: MenuComponent,
     private procesarPedidoService: ProcesarPedidoService,
-
-
-
-
-    ) {
-    // ...
-  }
+  ) {}
 
   ngOnInit() {
     // Suscribe al observable para obtener la cesta del servicio de intercambio de datos
@@ -33,13 +30,23 @@ export class PasarelaPagoComponent implements OnInit {
       this.cesta = cesta;
     });
   }
+
   ProcesarPedido() {
-    this.procesarPedidoService
-      .enviarPedido(this.cesta, this.menuComponent.cliente.usuario)
-      .subscribe({
-        next: r => alert('Pedido procesado'),
-        error: e => alert('El pedido no se ha preocesado')
-      });
+    if (this.verificarCampos()) {
+      this.procesarPedidoService
+        .enviarPedido(this.cesta, this.menuComponent.cliente.usuario)
+        .subscribe({
+          next: r => alert('Pedido procesado'),
+          error: e => alert('El pedido no se ha procesado')
+        });
+    } else {
+      alert('Por favor, complete todos los campos correctamente.');
+    }
   }
+
+    // En TypeScript
+    verificarCampos() {
+      return this.nombre && this.numero && this.expiracion && this.cvv;
+    }
 
 }
